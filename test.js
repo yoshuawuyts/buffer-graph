d40cc6e11939b603f694a0523d5425f198ac8300
Yoshua Wuyts <yoshuawuyts@gmail.com>
2017-08-06 00:08:09 +0200

    test

---
+++ test.js
@@ -0,0 +1,113 @@
+var tape = require('tape')
+var spok = require('spok')
+
+var bufferGraph = require('./')
+
+tape('should provide initial data', function (assert) {
+  assert.plan(2)
+
+  var graph = bufferGraph()
+  graph.node('first', function (data, edge) {
+    spok(assert, data, {
+      arguments: spok.object
+    })
+  })
+  graph.start({ hi: 'kittens' })
+})
+
+tape('should resolve a graph', function (assert) {
+  assert.plan(5)
+  var i = 0
+
+  var graph = bufferGraph()
+  graph.node('first', function (data, edge) {
+    assert.equal(i, 0, 'i = 0')
+    i++
+    edge('foo', Buffer.from('beep'))
+  })
+
+  graph.node('second', [ 'first:foo' ], function (data, edge) {
+    spok(assert, data, {
+      first: {
+        foo: {
+          buffer: spok.buffer,
+          hash: spok.string
+        }
+      }
+    })
+  })
+
+  graph.start()
+})
+
+tape('should resolve a graph with 2 dependencies', function (assert) {
+  assert.plan(1)
+  var i = 0
+
+  var graph = bufferGraph()
+  graph.node('first', function (data, edge) {
+    assert.equal(i, 0, 'i = 0')
+    i++
+    edge('foo', Buffer.from('beep'))
+  })
+
+  graph.node('second', [ 'first:foo', 'first:bar' ], function (data, edge) {
+    spok(assert, data, {
+      first: {
+        foo: {
+          buffer: spok.buffer,
+          hash: spok.string
+        },
+        bar: {
+          buffer: spok.buffer,
+          hash: spok.string
+        }
+      }
+    })
+  })
+
+  graph.start()
+})
+
+tape('emit events on change', function (assert) {
+  assert.plan(3)
+
+  var graph = bufferGraph()
+  graph.on('change', function (nodeName, edgeName, data) {
+    assert.equal(nodeName, 'first', 'nodeName was ok')
+    assert.equal(edgeName, 'first', 'edgeName was ok')
+    assert.ok(data[nodeName][edgeName])
+  })
+  graph.node('first', function (data, edge) {
+    edge('foo', Buffer.from('beep'))
+  })
+  graph.start()
+})
+
+tape('retrigger an event on change', function (assert) {
+  assert.plan(5)
+  var called = false
+  var i = 0
+
+  var graph = bufferGraph()
+  graph.node('first', function (data, edge) {
+    assert.equal(i, 0, 'i = 0')
+    i++
+    edge('foo', Buffer.from('beep'))
+    setTimeout(function () {
+      edge('foo', Buffer.from('boop'))
+    }, 100)
+  })
+
+  graph.node('second', [ 'first:foo' ], function (data, edge) {
+    if (!called) {
+      assert.equal(i, 1, 'i = 1')
+    } else {
+      assert.equal(i, 2, 'i = 2')
+    }
+    called = true
+    i++
+  })
+
+  graph.start()
+})
